d87f5ba61d7506d156858b46f2ffd59dad6090f4
Satyabrata Pradhan <99580536+satyabrata860@users.noreply.github.com>
2023-10-02 23:17:50 +0530

    Update TemplateSelect.tsx

--- src/modules/builder/nav-bar/components/TemplateSelect.tsx
+++ src/modules/builder/nav-bar/components/TemplateSelect.tsx
@@ -24,7 +24,7 @@ export const TemplateSelect = () => {
       <div>
         <OutlinedButton
           onClick={() => {
-            window.open('https://github.com/sadanandpai/resume-builder', '_blank');
+            window.open('https://github.com/satyabrata860/dev.resume.io', '_blank');
           }}
         >
           Contribute on Github
