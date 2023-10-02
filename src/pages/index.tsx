bfdb4a16477699d50fb0742a3a0c50f55237dcbe
Satyabrata Pradhan <99580536+satyabrata860@users.noreply.github.com>
2023-10-02 22:46:07 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const HomePage: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Web.DEV-Resume: Home</title>
+        <title>DEV-Resume.io:Home</title>
         <meta name="description" content="Single Page Resume Builder" />
         <link rel="icon" type="image/png" href="/icons/resume-icon.png" />
       </Head>
